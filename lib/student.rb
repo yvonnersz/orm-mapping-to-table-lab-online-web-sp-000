d08f404d5cb833f9662780b8910fa74194bf762a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 01:42:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name,grade)
+  def self.create(hash)
     student = Student.new(name,grade)
     student.save
     student
