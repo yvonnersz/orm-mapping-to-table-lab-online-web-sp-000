570585fcfec014b7acb38cc7ca0e25a09f38f2b6
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 01:39:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,9 @@ class Student
     DB[:conn].execute(sql,self.name,self.grade)
   end
 
-  def self.create
-
+  def self.create(name,grade)
+    student = Student.new(name,grade)
+    student.save
+    student
   end
 end
