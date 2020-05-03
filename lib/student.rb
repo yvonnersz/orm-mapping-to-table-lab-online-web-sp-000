caab8b741fa08b9e89a4580a0fa015d0dcc15b1a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 01:37:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
       INSERT INTO students(name,grade)
       VALUES (?,?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql,self.name,self.grade)
   end
 
 end
